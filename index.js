1c4ef7465f25527e11e04928d614dc22c91a07e4
hotman78 <hotmanww@gmail.com>
2020-10-12 23:17:18 +0900

    aaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function vs() {
     var url="https://script.google.com/macros/s/AKfycbzcQl4L4-Z-3DXZyUY2q3UzQx2UaoNy8knJp7J9tlsl52xlLFEU/exec?id=";
-    var p1=$("input#player1").value;
-    var p2=$("input#player2").value;
+    var p1=$("input#player1").val;
+    var p2=$("input#player2").val;
     var request1 = new XMLHttpRequest();
     var request2 = new XMLHttpRequest();
     request1.open('GET',url+p1,true);
