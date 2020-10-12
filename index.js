a22e70af1c03bd162009b32110fa9a8caf1097e3
hotman78 <hotmanww@gmail.com>
2020-10-12 23:58:01 +0900

    aaa

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ function callback(e) {
     console.log(e["AtCoder Regular Contest 105"]);
 }
 function vs() {
-    var url="https://script.google.com/macros/s/AKfycbzcQl4L4-Z-3DXZyUY2q3UzQx2UaoNy8knJp7J9tlsl52xlLFEU/exec?id=";
     {
+        var url="https://script.google.com/macros/s/AKfycbzcQl4L4-Z-3DXZyUY2q3UzQx2UaoNy8knJp7J9tlsl52xlLFEU/exec?id=";
         var p=$("input#player1").val();
         var script = document.createElement('script');
         script.src = url+p+'&callback=true';
         document.body.appendChild(script);
     }
     {
+        var url="https://script.google.com/macros/s/AKfycbzcQl4L4-Z-3DXZyUY2q3UzQx2UaoNy8knJp7J9tlsl52xlLFEU/exec?id=";
         var p=$("input#player2").val();
         var script = document.createElement('script');
         script.src = url+p+'&callback=true';
