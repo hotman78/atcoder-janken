93fc85f67cf8099042ff7319c56a21aff76e7e12
hotman78 <hotmanww@gmail.com>
2020-10-12 23:55:22 +0900

    aaa

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+function callback(e) {
+    console.log(e["AtCoder Regular Contest 105"]);
+}
 function vs() {
     var url="https://script.google.com/macros/s/AKfycbzcQl4L4-Z-3DXZyUY2q3UzQx2UaoNy8knJp7J9tlsl52xlLFEU/exec?id=";
-    var p1=$("input#player1").val();
-    var p2=$("input#player2").val();
-    var request1 = new XMLHttpRequest();
-    var request2 = new XMLHttpRequest();
-    request1.open('GET',url+p1,true);
-    request2.open('GET',url+p2,true);
-    var b1=false,b2=false;
-    request1.onload = function () {
-        b1=true;
-        console.log("aaa");
-        console.log(this.response);
-        $("body").append(this.response);
+    {
+        var p=$("input#player1").val();
+        var script = document.createElement('script');
+        script.src = 'url'+p+'&callback=true';
+        document.body.appendChild(script);
     }
-    request2.onload = function () {
-        b2=true;
-        $("body").append(this.response);
+    {
+        var p=$("input#player2").val();
+        var script = document.createElement('script');
+        script.src = 'url'+p+'&callback=true';
+        document.body.appendChild(script);
     }
-    request1.send();
-    request2.send();
 }
